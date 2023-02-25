650c5c7bc3080d136af2470a79b4ba75f20f3a36
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2023-02-25 16:08:38 +0900

    feat: code formatting

--- src/application/domain/user.ts
+++ src/application/domain/user.ts
@@ -1,37 +1,47 @@
 import { Character } from "./character";
 
 export class User {
-    id: number;
-    userName: string;
-    password: string;
-    characterList: Character[];
+  id: number;
+  userName: string;
+  password: string;
+  characterList: Character[];
 
-    constructor(id: number, userName: string, password: string, characterList?: Character[]) {
-        this.id = id;
-        this.userName = userName;
-        this.password = password;
-        this.characterList = characterList || [];
-    }
+  constructor(
+    id: number,
+    userName: string,
+    password: string,
+    characterList?: Character[]
+  ) {
+    this.id = id;
+    this.userName = userName;
+    this.password = password;
+    this.characterList = characterList || [];
+  }
 
-    addCharacter(character: Character) {
-        if(this.characterList.find((c) => c.isEqualTo(character)) === undefined) {
-            this.characterList.push(character);
-        }
+  addCharacter(character: Character) {
+    if (this.characterList.find((c) => c.isEqualTo(character)) === undefined) {
+      this.characterList.push(character);
     }
+  }
 
-    updateCharacter(character: Character) {
-        const updateCharacterId = this.characterList.findIndex(c => !c.isEqualTo(character));
-        this.characterList.splice(updateCharacterId, 0, character);
-    }
-    
-    removeCharacter(character: Character) {
-        if(this.characterList.find((c) => c.isEqualTo(character))){
-            this.characterList = this.characterList.filter((c) => c.id !== character.id);
-        }
-    }
+  updateCharacter(character: Character) {
+    const updateCharacterId = this.characterList.findIndex(
+      (c) => !c.isEqualTo(character)
+    );
+    this.characterList.splice(updateCharacterId, 0, character);
+  }
 
-    updateTodayComment(character: Character, message: string) {
-        this.characterList.find(c => c.id === character.id)?.updateComment(message);
+  removeCharacter(character: Character) {
+    if (this.characterList.find((c) => c.isEqualTo(character))) {
+      this.characterList = this.characterList.filter(
+        (c) => c.id !== character.id
+      );
     }
+  }
 
+  updateTodayComment(character: Character, message: string) {
+    this.characterList
+      .find((c) => c.id === character.id)
+      ?.updateComment(message);
+  }
 }
